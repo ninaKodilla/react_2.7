fc9d13cf75a4f6280428268f9ddd47a106ae9c1f
npupiec <nkozlowska89@gmail.com>
2018-05-27 15:12:44 +0200

    code for task 5

---
+++ components/Contacts.js
@@ -0,0 +1,13 @@
+var Contacts = React.createClass({
+    propTypes: {
+      items: React.PropTypes.array.isRequired,
+    },
+  
+    render: function() {
+      var contacts = this.props.items.map(function(contact) {
+          return (<Contact key={contact.id} item={contact}></Contact>)
+      });
+      
+      return (<ul className={'contactsList'}>{contacts}</ul>)
+    }
+});
